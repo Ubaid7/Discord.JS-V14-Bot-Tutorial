628e35a01882d47fd58f24d81f6d175355978c9d
Ubaid7 <75165174+Ubaid7@users.noreply.github.com>
2023-04-19 09:47:36 +0300

    Add files via upload

--- Episode 15/Commands/Info/uptime.js
+++ Episode 15/Commands/Info/uptime.js
@@ -1,1 +1,33 @@
+const { SlashCommandBuilder, EmbedBuilder } = require('discord.js')
 
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName('uptime') // Name Of Slash Command
+        .setDescription('Get UpTime Of Bot') // Description Of Slash Command
+        .setDMPermission(false),
+
+    async execute(interaction, client) {
+
+        const days = Math.floor(client.uptime / 86400000)
+        const hours = Math.floor(client.uptime / 3600000) % 24 // 1 Day = 24 Hours
+        const minutes = Math.floor(client.uptime / 60000) % 60 // 1 Hour = 60 Minutes
+        const seconds = Math.floor(client.uptime / 1000) % 60 // I Minute = 60 Seconds
+
+        const uptimeEmbed = new EmbedBuilder()
+            .setAuthor({
+                name: `UpTime Of ${client.user.username}`,
+                iconURL: client.user.displayAvatarURL()
+            })
+            .setColor('Random')
+            .setDescription(`UpTime Of ${client.user.username} Is \n\`${days}\` Days \`${hours}\` Hours \`${minutes}\` Minutes \`${seconds}\` Seconds`)
+            .setFooter({
+                text: `UpTime`,
+                iconURL: client.user.displayAvatarURL()
+            })
+            .setTimestamp()
+
+        await interaction.deferReply({ fetchReply: true })
+        await interaction.editReply({ embeds: [uptimeEmbed] })
+
+    }
+}
